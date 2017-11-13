c21d4a3501e48caa00f60cba6b9540fad0d123d5
vladislav.dits <vladislav.dits@crystalnix.com>
2017-11-13 18:38:55 +0600

    Fix comments style.

--- app/scripts/components/Body/Book/Article/style.js
+++ app/scripts/components/Body/Book/Article/style.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'aphrodite'
 const articleStyles = StyleSheet.create({
   paper: {
     marginTop: '30px',
+    paddingBottom: '50px',
   },
   paperMobile: {
     padding: '15px',
